092310dab8b510d2c9814cfa9937356c33c8daf4
Jude Clark <judeclark2019@gmail.com>
2020-09-28 19:30:20 -0400

    manager test suite passed

--- Develop/test/Manager.test.js
+++ Develop/test/Manager.test.js
@@ -1,8 +1,7 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-//ONLY
-test.only("Can set office number via constructor argument", () => {
+test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
@@ -14,7 +13,7 @@ test('getRole() should return "Manager"', () => {
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get office number via getOffice()", () => {
+test("Can get office number via getOfficeNumber()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);

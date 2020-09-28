073aee1d11d916310c11b28832dbad6bfd8580ef
Jude Clark <judeclark2019@gmail.com>
2020-09-28 19:26:11 -0400

    intern test suite pass

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,7 +1,18 @@
+const Employee = require("../lib/Employee");
+
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-class Intern {
-    constructor(school){
+class Intern extends Employee {
+    constructor(name, id, email, school){
+        super(name, id, email);
         this.school = school;
+    }
+
+    getRole(){
+        return "Intern";
+    }
+
+    getSchool(){
+        return this.school;
     }
 }
 
